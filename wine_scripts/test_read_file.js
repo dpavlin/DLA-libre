264d49d75f8cf7d92f4266207860bad186aa5965
Dobrica Pavlinusic <dpavlin@rot13.org>
2026-07-25 12:03:05 +0200

    wine_scripts: remove blob commit - will re-add individually

--- wine_scripts/test_read_file.js
+++
@@ -1,8 +0,0 @@
-var fso = new ActiveXObject("Scripting.FileSystemObject");
-try {
-    var file = fso.OpenTextFile("D:\\DLA\\ShelfOrderList\\A.tab", 1);
-    WScript.Echo("Line 1: " + file.ReadLine());
-    file.Close();
-} catch (e) {
-    WScript.Echo("Error reading file: " + e.message);
-}
